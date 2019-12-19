dc7a75d518c51c8173681b67dbdac296fc10d36d
Mikhail <pms_03@mail.ru>
2019-12-19 10:18:03 +0800

    Slider Beggining

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const selectorButton = {
+	next: document.querySelector('.slider-navigation-button.next'),
+	previous: document.querySelector('.slider-navigation-button.previous.previous')
+}
+
+swipeNext()=>{
+	let slides = {
+		centerLeft: document.querySelector('.slider-center');
+		leftRight: document.querySelector('.slider-left');
+		rightCenter: document.querySelector('.slider-right');
+	}
+	
+}
+
