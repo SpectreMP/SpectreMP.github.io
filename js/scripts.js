92f443d98935e2209fab8e1f8c0544d9bfbac9ea
Mikhail <pms_03@mail.ru>
2019-12-22 22:15:01 +0800

    after slider rewrite

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,28 @@ const navigationButton = {
 	next: document.querySelector('.slider-navigation-button.next'),
 	previous: document.querySelector('.slider-navigation-button.previous.previous')
 }
+let currentNumber = 0;
+const slides = document.querySelectorAll('.slider-item');
+function swipe(page){
 
-function swipeNext(){
-	let slides = {
-		rightCenter: document.querySelector('.slider-right'),
-		centerLeft: document.querySelector('.slider-center'),
-		leftRight: document.querySelector('.slider-left'),
-	}
-	slides.centerLeft.classList.add("slider-left","slider-fix")
-	slides.centerLeft.classList.remove("slider-center")
-	slides.leftRight.classList.add("slider-right")
-	slides.leftRight.classList.remove("slider-left","slider-fix")
-	slides.rightCenter.classList.add("slider-center")
-	slides.rightCenter.classList.remove("slider-right")
+	Array.from(slides).map(function(el,num){
+		if(num>page){
+			el.classList.remove("slider-left","slider-center")
+			el.classList.add("slider-right")
+		} else if (num == page) {
+			el.classList.remove("slider-left","slider-right")
+			el.classList.add("slider-center")
+		} else {
+			el.classList.remove("slider-right","slider-center")
+			el.classList.add("slider-left")
+		}
+	});
+	currentNumber = page
 }
-function swipePrevious(){
-	let slides = {
-		centerRight: document.querySelector('.slider-center'),
-		leftCenter: document.querySelector('.slider-left'),
-		rightLeft: document.querySelector('.slider-right')
-	}
-	slides.centerRight.classList.add("slider-right","slider-fix")
-	slides.centerRight.classList.remove("slider-center")
-	slides.leftCenter.classList.add("slider-center")
-	slides.leftCenter.classList.remove("slider-left")
-	slides.rightLeft.classList.add("slider-left")
-	slides.rightLeft.classList.remove("slider-right","slider-fix")
-}
-navigationButton.next.onclick = swipeNext
-navigationButton.previous.onclick = swipePrevious
+
+swipe(currentNumber)
+navigationButton.next.addEventListener("click", function(){swipe(currentNumber==slides.length-1?0:currentNumber+1)})
+navigationButton.previous.addEventListener("click", function(){swipe(currentNumber==0?slides.length-1:currentNumber-1)})
 
 const sliderScreen = document.querySelector('.slider');
 let touch = {
@@ -45,9 +38,9 @@ sliderScreen.addEventListener("touchend", function(evt){
 	touch.finalX = evt.changedTouches[0].screenX
 	touch.distance = touch.finalX - touch.startX
 	if (touch.distance<screen.width*-0.15) {
-		swipeNext()
+		swipe(currentNumber==slides.length-1?0:currentNumber+1)
 	}
 	if (touch.distance>screen.width*0.15){
-		swipePrevious()
+		swipe(currentNumber==0?slides.length-1:currentNumber-1)
 	}
 })
